fbf76a2f5ce969521d4200e712381d1d0531233f
FatimaZekic <fzekic02@gmail.com>
2022-12-19 19:22:37 -0500

    Initial Commit

---
+++ TriviaGameFinal/TriviaGameFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameFinalApp.swift
+//  TriviaGameFinal
+//
+//  Created by Fatima Zekic on 12/19/22.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
